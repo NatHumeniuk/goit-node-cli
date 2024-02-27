aec4a864a359f9b821c584a4e2a4871cb100805a
NatHumeniuk <nat.gumenuk@gmail.com>
2024-02-27 21:45:07 +0200

    tasks

---
+++ index.js
@@ -0,0 +1,42 @@
+import { program } from "commander";
+import * as contactsService from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      return console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactsService.addContact(name, email, phone);
+      return console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contactsService.removeContact(id);
+      return console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
